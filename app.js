64d98009b7d4b665ccd1eedb935b02710c4c7052
shashankranjan-dev <shashankranjan970832@gmail.com>
2025-07-09 21:49:40 +0530

    port updated

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(userRoutes);
 if (process.env.NODE_ENV !== "test") {
-  const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 5005;
   app.listen(PORT, () => console.log(`Boyaah, Server running on port ${PORT}`));
 }
 export default app;
